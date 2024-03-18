13fa701c05d60dc6427bc4af9f1a3f5ef465041e
oahuoahu <oahuoahu@nate.com>
2024-03-19 01:24:51 +0900

    네이버 웹사이트에서 로그인 페이지 버튼 클릭 자동화

--- Include/test.py
+++ Include/test.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
+from selenium.webdriver.common.by import By
 
 # 브라우저 꺼짐 방지 옵션
 chrome_options = Options()
@@ -13,3 +13,7 @@ driver = webdriver.Chrome(options=chrome_options)
 
 url = 'https://www.naver.com'
 driver.get(url)
+
+
+login_page_btn = driver.find_element(By.XPATH,'//*[@id="account"]/div/a')
+login_page_btn.click()
