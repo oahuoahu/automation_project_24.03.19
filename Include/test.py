0a226df2284e844d88071cad0b90669da4549841
oahuoahu <oahuoahu@nate.com>
2024-03-19 01:43:28 +0900

    로그인 아이디, 비밀번호 입력받고 자동로그인 기능 구현

--- Include/test.py
+++ Include/test.py
@@ -1,3 +1,4 @@
+import getpass
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
@@ -14,6 +15,29 @@ driver = webdriver.Chrome(options=chrome_options)
 url = 'https://www.naver.com'
 driver.get(url)
 
-
+#로그인 페이지 이동 버튼 클릭
 login_page_btn = driver.find_element(By.XPATH,'//*[@id="account"]/div/a')
 login_page_btn.click()
+
+
+
+user_id = input('아이디를 입력해주세요 : ')
+user_pw = getpass.getpass('비밀번호를 입력해주세요 : ')
+
+
+
+# 아이디와 비밀번호를 입력
+user_id_input = driver.find_element(By.ID,"id")
+user_pw_input = driver.find_element(By.ID,"pw")
+
+user_id_input.send_keys(user_id)
+user_pw_input.send_keys(user_pw)
+
+# https://donghodazzi.tistory.com/305
+# https://wikidocs.net/91474
+# https://donghodazzi.tistory.com/305
+# https://github.com/oahuoahu/automation_project_24.03.19
+
+#fhrmdlsqjxms
+login_btn = driver.find_element(By.XPATH,'//*[@id="log.login"]')
+login_btn.click()
